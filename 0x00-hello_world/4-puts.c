5838d87cdcd837d8d373a8caa0afa7ed7dfbb2a1
kalutesfay <tesfaykalu@gmail.com>
2021-06-24 11:37:37 -0700

    6th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ *puts - print out the given text while addin a new line
+ *Return:ends the execution of the function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
