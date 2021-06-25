4c5d978577c2aa45704b196e4f3c124c4dfe7c83
kalutesfay <tesfaykalu@gmail.com>
2021-06-25 06:31:52 -0700

    7th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Description: Prints "with proper grammer, but the outcome is a piece of art,\n"
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
