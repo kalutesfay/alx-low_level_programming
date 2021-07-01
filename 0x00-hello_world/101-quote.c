4d385c6b9183ed2709cee456e4aa4916c4b767f6
kalutesfay <tesfaykalu@gmail.com>
2021-07-01 09:46:19 -0700

    using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints string
+ * Description: prints the given string by calling putchar
+ * Return: 1
+ */
+int main(void)
+{
+       char ch = "and that piece of art is useful\" - Dora korpar, 2015-10-19"
+      
+       putchar(ch);
+      
+       return(1);
+}
